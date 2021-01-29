9b8732761824f78286d56d33fc2c6c309714225e
Nora <50810398+Nooraje@users.noreply.github.com>
2021-01-29 09:48:41 +0300

    Add files via upload

---
+++ commands/link.js
@@ -0,0 +1,24 @@
+const link = require("../links.json")
+const Discord = require("discord.js");
+const fs = require('fs');
+
+module.exports = {
+	name: 'link',
+	aliases: ['linkosu', 'linkprofile'],
+	cooldown: 2.5,
+	description: 'link!',
+	args: true,
+	usage: '<nickname>',
+	guildOnly: true,
+	permissions: false,
+	execute(message, args) {
+			var nick = args[0]
+			link[message.author.id] = {
+				nick: nick
+			}
+			fs.writeFile("./links.json", JSON.stringify(link, null, 4), err => {
+				if (err) throw err
+				message.channel.send("Your nickname linked as " + `**${nick}**`)
+			})
+	},
+};
