92e83c7743ae5d2e046d7ceee9230d47ec35be5c
Nora <50810398+Nooraje@users.noreply.github.com>
2021-02-03 17:42:53 +0300

    Update osu.js

--- commands/osu.js
+++ commands/osu.js
@@ -1,7 +1,8 @@
 const link = require("../jsons/links.json")
-const request = require("request");
 const config = require("../jsons/configs.json")
 const Discord = require("discord.js");
+
+let api = require('../osuapi.js');
 
 module.exports = {
     name: 'osu',
@@ -13,64 +14,57 @@ module.exports = {
     guildOnly: true,
     permissions: false,
     execute(message, args) {
+        //my stupid way to get username
         try {
-            if (args[0]) {
-                var username = args[0]
+            if (!args[0]) {
+                username = link[message.author.id].nick
+            } else if (args[0].length > 20) {
+                let mention = message.mentions.users.first();
+                username = link[mention.id].nick
             } else {
-                var username = link[message.author.id].nick
+                username = args[0]
             }
         } catch {
             message.channel.send("Please link yourself. **!link <nick>**")
             return
         }
-        request(
-            "https://osu.ppy.sh/api/get_user?k=" + config.apikey + `&u=${username}`,
-            function(error, response, body) {
-                randomnumber = Math.floor(Math.random() * 10000);
-                r = body;
-                obj = JSON.parse(r);
-                //api callbacks
-                try {
-                    osuprofilepicture = "http://s.ppy.sh/a/" + obj[0]["user_id"];
-                    countryflagicon = "https://www.countryflags.io/" + obj[0]["country"] + "/flat/64.png";
-                    username = obj[0]["username"]
-                    osuprofilelink = "https://osu.ppy.sh/u/" + username
-                    pp_rank =
-                        "**» Rank:** " +
-                        "#" +
-                        obj[0]["pp_rank"] +
-                        " [" +
-                        obj[0]["country"] +
-                        "#" +
-                        obj[0]["pp_country_rank"] +
-                        "]";
-                    pp_raw = obj[0]["pp_raw"];
-                    accuracy = obj[0]["accuracy"] / 1;
-                    accuracyy = accuracy.toFixed(2);
-                    playcount = "**» Playcount:** " + obj[0]["playcount"]
-                    hoursplayedmath = obj[0]["total_seconds_played"] / 60 / 60;
-                    hoursplayed = "**» Hours Played:** " + hoursplayedmath.toFixed(0);
-                    joindate = obj[0]["join_date"]
-                    //description
-                    asd =
-                        `**» Username:** ${username}\n` +
-                        `${pp_rank}\n` +
-                        `**» Total PP:** ${pp_raw}\n` +
-                        `**» Hit Accuracy:** ${accuracyy}%\n` +
-                        `${playcount}\n` +
-                        `${hoursplayed}`;
-                    //embed
-                    embed = new Discord.MessageEmbed()
-                        .setAuthor("osu! Profile", countryflagicon, osuprofilelink)
-                        .setDescription(asd)
-                        .setThumbnail(osuprofilepicture + `?${randomnumber}`)
-                        .setColor("#1db35e")
-                        .setFooter("»» Join Date: " + joindate);
-                    message.channel.send(embed);
-                } catch (err) {
-                    message.channel.send("Something went wrong.. Please specify a correct nickname.")
-                }
-            }
-        );
+        api.getuser(username).then(getuser => {
+            osuprofilepicture = "http://s.ppy.sh/a/" + getuser[0]["user_id"];
+            countryflagicon = "https://www.countryflags.io/" + getuser[0]["country"] + "/flat/64.png";
+            username = getuser[0]["username"]
+            osuprofilelink = "https://osu.ppy.sh/u/" + username
+            pp_rank =
+                "**» Rank:** " +
+                "#" +
+                getuser[0]["pp_rank"] +
+                " [" +
+                getuser[0]["country"] +
+                "#" +
+                getuser[0]["pp_country_rank"] +
+                "]";
+            pp_raw = getuser[0]["pp_raw"];
+            accuracy = getuser[0]["accuracy"] / 1;
+            accuracyy = accuracy.toFixed(2);
+            playcount = "**» Playcount:** " + getuser[0]["playcount"]
+            hoursplayedmath = getuser[0]["total_seconds_played"] / 60 / 60;
+            hoursplayed = "**» Hours Played:** " + hoursplayedmath.toFixed(0);
+            joindate = getuser[0]["join_date"]
+            desc =
+                `**» Username:** ${username}\n` +
+                `${pp_rank}\n` +
+                `**» Total PP:** ${pp_raw}\n` +
+                `**» Hit Accuracy:** ${accuracyy}%\n` +
+                `${playcount}\n` +
+                `${hoursplayed}`;
+            embed = new Discord.MessageEmbed()
+                .setAuthor("osu! Profile for " + `${username}`, countryflagicon, osuprofilelink)
+                .setDescription(desc)
+                .setThumbnail(osuprofilepicture + `?${api.randomnumber(10000)}`)
+                .setColor(message.member.displayHexColor)
+                .setFooter("»» Join Date: " + joindate);
+            message.channel.send(embed);
+        }).catch(err => {
+            message.channel.send("Something went wrong.. Please specify a correct nickname.")
+        });
     },
 };
