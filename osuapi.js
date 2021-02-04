44be3f014e735b3362b6aa78359e01ebd095d410
Nora <50810398+Nooraje@users.noreply.github.com>
2021-02-04 18:53:10 +0300

    Update osuapi.js

--- osuapi.js
+++ osuapi.js
@@ -1,5 +1,7 @@
 const rp = require('request-promise');
 const config = require("./jsons/configs.json")
+const request = require('request-promise-native')
+const {Beatmap, Osu: {DifficultyCalculator, PerformanceCalculator}} = require('osu-bpdpc')
 
 api_key = config.apikey
 base_api = "https://osu.ppy.sh/api/"
@@ -9,12 +11,40 @@ beatmap_api_url = `${base_api}` + "get_beatmaps"
 user_best_url = `${base_api}` + "get_user_best"
 beatmap_scores = `${base_api}` + "get_scores"
 
-function getuser(username) {
-    getuserlink = `${user_api_url}?k=${api_key}&u=${username}`
-    return rp(getuserlink).then(body => {
+function get_user(username) {
+    user_url = `${user_api_url}?k=${api_key}&u=${username}`
+    return rp(user_url).then(body => {
         obj = JSON.parse(body)
         return obj;
     });
+}
+
+function get_user_recent(username) {
+    recent_url = `${recent_api_url}?k=${api_key}&u=${username}`
+    return rp(recent_url).then(body => {
+        obj = JSON.parse(body)
+        return obj;
+    });
+}
+
+function get_pp(beatmapid, maxcombo, count50, count100, count300, countMiss, countKatu, countGeki, perfect, mods) {
+    request.get('https://osu.ppy.sh/osu/' + `${beatmapid}`).then(osu => {
+        let beatmap = Beatmap.fromOsu(osu)
+        let score = {
+          maxcombo: maxcombo,
+          count50: count50,
+          count100: count100,
+          count300: count300,
+          countMiss: countMiss,
+          countKatu: countKatu,
+          countGeki: countGeki,
+          perfect: perfect,
+          mods: mods,
+        }
+        diffCalc = DifficultyCalculator.use(beatmap).setMods(score.mods).calculate()
+        perfCalc = PerformanceCalculator.use(diffCalc).calculate(score).totalPerformance.toFixed(2)
+        return perfCalc
+      })
 }
 
 function randomnumber(x) {
@@ -23,5 +53,7 @@ function randomnumber(x) {
 
 module.exports = {
     randomnumber,
-    getuser
+    get_user,
+    get_user_recent,
+    get_pp
 }
