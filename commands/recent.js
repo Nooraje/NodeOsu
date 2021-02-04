d07fdefb20e3643a3bb1a7a246bf314cb66bca06
Nora <50810398+Nooraje@users.noreply.github.com>
2021-02-04 18:53:27 +0300

    Update recent.js

--- commands/recent.js
+++ commands/recent.js
@@ -1,7 +1,9 @@
 const link = require("../jsons/links.json")
-const request = require("request");
 const config = require("../jsons/configs.json")
 const Discord = require("discord.js");
+const {Beatmap, Osu: {DifficultyCalculator, PerformanceCalculator}} = require('osu-bpdpc')
+
+let api = require('../osuapi.js');
 
 module.exports = {
     name: 'recent',
@@ -13,25 +15,23 @@ module.exports = {
     guildOnly: true,
     permissions: false,
     execute(message, args) {
+        //my stupid way to get username
         try {
-            if (args[0]) {
-                var username = args[0]
+            if (!args[0]) {
+                username = link[message.author.id].nick
+            } else if (args[0].length > 20) {
+                let mention = message.mentions.users.first();
+                username = link[mention.id].nick
             } else {
-                var username = link[message.author.id].nick
+                username = args[0]
             }
         } catch {
             message.channel.send("Please link yourself. **!link <nick>**")
             return
         }
-        request(
-            "https://osu.ppy.sh/api/get_user_recent?k=" + config.apikey + `&u=${username}`,
-            function(error, response, body) {
-                randomnumber = Math.floor(Math.random() * 10000);
-                r = body;
-                obj = JSON.parse(r);
-                //api callbacks
-
-            }
-        );
+        api.get_user_recent(username).then(getuser => {
+            text = api.get_pp(getuser[0]["beatmap_id"], getuser[0]["maxcombo"], getuser[0]["count50"], getuser[0]["count100"], getuser[0]["count300"], getuser[0]["countmiss"], getuser[0]["countkatu"], getuser[0]["countgeki"], getuser[0]["perfect"], getuser[0]["enabled_mods"])
+            console.log(text)
+        })
     },
 };
