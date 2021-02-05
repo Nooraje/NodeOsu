05fb8859f9afde709f58ce074ed4256a45e2b34b
Nora <50810398+Nooraje@users.noreply.github.com>
2021-02-05 18:37:55 +0300

    Add files via upload

--- osuapi.js
+++ osuapi.js
@@ -1,7 +1,6 @@
 const rp = require('request-promise');
 const config = require("./jsons/configs.json")
-const request = require('request-promise-native')
-const {Beatmap, Osu: {DifficultyCalculator, PerformanceCalculator}} = require('osu-bpdpc')
+const { Beatmap, Osu: { DifficultyCalculator, PerformanceCalculator } } = require('osu-bpdpc')
 
 api_key = config.apikey
 base_api = "https://osu.ppy.sh/api/"
@@ -14,37 +13,48 @@ beatmap_scores = `${base_api}` + "get_scores"
 function get_user(username) {
     user_url = `${user_api_url}?k=${api_key}&u=${username}`
     return rp(user_url).then(body => {
-        obj = JSON.parse(body)
-        return obj;
+        try {
+            obj = JSON.parse(body)
+            return obj;
+        } catch {
+            return []
+        }
     });
 }
 
 function get_user_recent(username) {
     recent_url = `${recent_api_url}?k=${api_key}&u=${username}`
     return rp(recent_url).then(body => {
-        obj = JSON.parse(body)
-        return obj;
+        try {
+            obj = JSON.parse(body)
+            return obj;
+        } catch {
+            return []
+        }
     });
 }
 
 function get_pp(beatmapid, maxcombo, count50, count100, count300, countMiss, countKatu, countGeki, perfect, mods) {
-    request.get('https://osu.ppy.sh/osu/' + `${beatmapid}`).then(osu => {
+    beatmaplink = `https://osu.ppy.sh/osu/${beatmapid}`
+    return rp(beatmaplink).then(osu => {
         let beatmap = Beatmap.fromOsu(osu)
         let score = {
-          maxcombo: maxcombo,
-          count50: count50,
-          count100: count100,
-          count300: count300,
-          countMiss: countMiss,
-          countKatu: countKatu,
-          countGeki: countGeki,
-          perfect: perfect,
-          mods: mods,
+            maxcombo: maxcombo,
+            count50: count50,
+            count100: count100,
+            count300: count300,
+            countMiss: countMiss,
+            countKatu: countKatu,
+            countGeki: countGeki,
+            perfect: perfect,
+            mods: mods,
         }
         diffCalc = DifficultyCalculator.use(beatmap).setMods(score.mods).calculate()
         perfCalc = PerformanceCalculator.use(diffCalc).calculate(score).totalPerformance.toFixed(2)
-        return perfCalc;
-      })
+        console.log(beatmaplink)
+        console.log(score)
+        return perfCalc
+    })
 }
 
 function randomnumber(x) {
