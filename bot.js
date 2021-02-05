1dad7d3b259a7c34aa6c2e152b9d6f03d1c0d9ea
Nora <50810398+Nooraje@users.noreply.github.com>
2021-02-06 02:36:38 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -10,8 +10,8 @@ const cooldowns = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
 }
 
 client.once('ready', () => {
@@ -25,13 +25,13 @@ client.on('message', message => {
     } catch {
         return;
     }
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
 
-    const command = client.commands.get(commandName)
-        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    const command = client.commands.get(commandName) ||
+        client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
     if (!command) return;
 
@@ -50,7 +50,7 @@ client.on('message', message => {
         let reply = `You didn't provide any arguments, ${message.author}!`;
 
         if (command.usage) {
-        reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
         }
 
         return message.channel.send(reply);
@@ -59,14 +59,14 @@ client.on('message', message => {
     if (!cooldowns.has(command.name)) {
         cooldowns.set(command.name, new Discord.Collection());
     }
-    
+
     const now = Date.now();
     const timestamps = cooldowns.get(command.name);
     const cooldownAmount = (command.cooldown || 3) * 1000;
-    
+
     if (timestamps.has(message.author.id)) {
         const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-    
+
         if (now < expirationTime) {
             const timeLeft = (expirationTime - now) / 1000;
             return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
--- osuapi.js
+++ osuapi.js
@@ -1,6 +1,12 @@
 const rp = require('request-promise');
 const config = require("./jsons/configs.json")
-const { Beatmap, Osu: { DifficultyCalculator, PerformanceCalculator } } = require('osu-bpdpc')
+const {
+    Beatmap,
+    Osu: {
+        DifficultyCalculator,
+        PerformanceCalculator
+    }
+} = require('osu-bpdpc')
 
 api_key = config.apikey
 base_api = "https://osu.ppy.sh/api/"
@@ -34,36 +40,119 @@ function get_user_recent(username) {
     });
 }
 
-function get_pp(beatmapid, maxcombo, count50, count100, count300, countMiss, countKatu, countGeki, perfect, mods) {
+function get_beatmap(beatmapid) {
+    beatmap_url = `${beatmap_api_url}?k=${api_key}&b=${beatmapid}`
+    return rp(beatmap_url).then(body => {
+        try {
+            obj = JSON.parse(body)
+            return obj;
+        } catch {
+            return []
+        }
+    });
+}
+
+
+function get_pp(beatmapid, maxcombo, count50, count100, count300, countmiss, perfect, mods) {
     beatmaplink = `https://osu.ppy.sh/osu/${beatmapid}`
     return rp(beatmaplink).then(osu => {
         let beatmap = Beatmap.fromOsu(osu)
         let score = {
-            maxcombo: maxcombo,
-            count50: count50,
-            count100: count100,
-            count300: count300,
-            countMiss: countMiss,
-            countKatu: countKatu,
-            countGeki: countGeki,
-            perfect: perfect,
-            mods: mods,
+            maxcombo: maxcombo * 1,
+            count50: count50 * 1,
+            count100: count100 * 1,
+            count300: count300 * 1,
+            countMiss: countmiss * 1,
+            perfect: perfect * 1,
+            mods: mods * 1,
         }
         diffCalc = DifficultyCalculator.use(beatmap).setMods(score.mods).calculate()
-        perfCalc = PerformanceCalculator.use(diffCalc).calculate(score).totalPerformance.toFixed(2)
-        console.log(beatmaplink)
-        console.log(score)
+        perfCalc = PerformanceCalculator.use(diffCalc).calculate(score).totalPerformance.toFixed(0)
+        //console.log(beatmaplink)
+        //console.log(score)
         return perfCalc
     })
 }
 
+function num_to_mod(num) {
+    data = []
+    if(num == 0) data.push("No Mod,") 
+    return data
+}
+
+function get_rank_emote(rank) {
+    if(rank == "A") {
+        return "<:A_Emote:783829607982956565>"
+    }
+    if(rank == "B") {
+        return "<:B_Emote:783829608222425138>"
+    }
+    if(rank == "C") {
+        return "<:C_Emote:783829608235401216>"
+    }
+    if(rank == "F") {
+        return "<:F_Emote:783826073997148202>"
+    }
+}
+
+function get_if_fc_pp(beatmapid, maxcombo, count50, count100, count300, perfect, mods) {
+    beatmaplink = `https://osu.ppy.sh/osu/${beatmapid}`
+    return rp(beatmaplink).then(osu => {
+        let beatmap = Beatmap.fromOsu(osu)
+        let score = {
+            maxcombo: maxcombo * 1,
+            count50: count50 * 1,
+            count100: count100 * 1,
+            count300: count300 * 1,
+            countMiss: 0,
+            perfect: perfect * 1,
+            mods: mods * 1,
+        }
+        diffCalc = DifficultyCalculator.use(beatmap).setMods(score.mods).calculate()
+        perfCalc = PerformanceCalculator.use(diffCalc).calculate(score).totalPerformance.toFixed(0)
+        console.log(beatmaplink)
+        //console.log(score)
+        return perfCalc
+    })
+}
+
+function accuracyCalc(c300, c100, c50, misses) {
+    var totalHits = c300 * 1 + c100 * 1 + c50 * 1 + misses * 1;
+    var accuracy = 0.0;
+    if (totalHits > 0) {
+        accuracy = (c50 * 50.0 + c100 * 100.0 + c300 * 300.0) /
+            (totalHits * 300.0) * 100;
+    }
+    return accuracy.toFixed(2)
+};
+
 function randomnumber(x) {
     return Math.floor(Math.random() * x);
+}
+
+function get_fifty_emote() {
+    return "<:50_emote:783826074118127636>"
+}
+
+function get_onehundred_emote() {
+    return "<:100_Emote:783826074135429150>"
+}
+
+function get_miss_emote() {
+    return "<:Miss_Emote:783826074052067369>"
 }
 
 module.exports = {
     randomnumber,
     get_user,
     get_user_recent,
-    get_pp
+    get_pp,
+    accuracyCalc,
+    get_if_fc_pp,
+    get_beatmap,
+    num_to_mod,
+    get_rank_emote,
+    get_onehundred_emote,
+    get_fifty_emote,
+    get_miss_emote
 }
