a36ddc41772062d4ee2b2ac2d09f1034165be1f0
Nora <50810398+Nooraje@users.noreply.github.com>
2021-02-05 18:37:28 +0300

    Add files via upload

--- commands/recent.js
+++ commands/recent.js
@@ -11,7 +11,7 @@ module.exports = {
     usage: '<nickname>',
     guildOnly: true,
     permissions: false,
-    execute(message, args) {
+    execute(message, args){
         //my stupid way to get username
         try {
             if (!args[0]) {
@@ -23,12 +23,16 @@ module.exports = {
                 username = args[0]
             }
         } catch {
-            message.channel.send("Please link yourself. **!link <nick>**")
-            return
+            return message.channel.send("Please link yourself. **!link <nick>**")
         }
         api.get_user_recent(username).then(getuser => {
-            text = api.get_pp(getuser[0]["beatmap_id"], getuser[0]["maxcombo"], getuser[0]["count50"], getuser[0]["count100"], getuser[0]["count300"], getuser[0]["countmiss"], getuser[0]["countkatu"], getuser[0]["countgeki"], getuser[0]["perfect"], getuser[0]["enabled_mods"])
-            console.log(text)
-        })
+            api.get_pp(getuser[0]["beatmap_id"], getuser[0]["maxcombo"], getuser[0]["count50"], getuser[0]["count100"], getuser[0]["count300"], getuser[0]["countmiss"], getuser[0]["countkatu"], getuser[0]["countgeki"], getuser[0]["perfect"], getuser[0]["enabled_mods"])
+            .then(function(result){
+                message.channel.send(result)
+                console.log(getuser[0])
+            })
+        }).catch(err => {
+            return message.channel.send("Something went wrong.. Please specify a correct nickname.")
+        });
     },
 };
