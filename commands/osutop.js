f0f4a699f5f4644bffa37bd11b34021a71df5ccf
Nora <50810398+Nooraje@users.noreply.github.com>
2021-02-11 04:57:21 +0300

    Add files via upload

--- commands/osutop.js
+++ commands/osutop.js
@@ -27,7 +27,6 @@ module.exports = {
         let promise = new Promise((res, rej) => {
             api.get_user_best(username).then(getuserbest => {
                 api.get_user(username).then(function (getuser) {
-                    console.log(getuserbest[2]["rank"])
                     var osutopdesc = ``
                     username = getuser[0]["username"]
                     pp_raw = getuser[0]["pp_raw"];
@@ -35,7 +34,8 @@ module.exports = {
                     countryflagicon = "https://www.countryflags.io/" + getuser[0]["country"] + "/flat/64.png";
                     let promises = [];
                     for (i = 0; i < 3; i++) {
-                        promises.push(api.get_beatmap(getuserbest[i]["beatmap_id"] * 1, getuserbest[i]["enabled_mods"] * 1))
+                        enabled_mods = api.num_to_num_but_diff_increase_mods_only(parseInt(getuserbest[i]["enabled_mods"]))
+                        promises.push(api.get_beatmap(getuserbest[i]["beatmap_id"] * 1, enabled_mods))
                     }
                     Promise.all(promises).then(resp => {
                         let i = 0;
@@ -50,26 +50,35 @@ module.exports = {
                             score = getuserbest[i]["score"] * 1
                             acc = api.accuracyCalc(getuserbest[i]["count300"], getuserbest[i]["count100"], getuserbest[i]["count50"], getuserbest[i]["countmiss"])
                             osutopdesc += `**${i + 1}. [${getbeatmap[0]["title"]} [${getbeatmap[0]["version"]}]](${beatmapsetlink})**\n▸ **[${star.toFixed(2)}★]** +${mod} | ${score.toLocaleString()} - ${api.get_rank_emote(getuserbest[i]["rank"])}\n▸ **${(parseInt(getuserbest[i]["pp"]))}**pp | **x${getuserbest[i]["maxcombo"]}/${getbeatmap[0]["max_combo"] * 1}**\n▸ ${acc}% | ${getuserbest[i]["count100"]}x${api.get_onehundred_emote()} | ${getuserbest[i]["count50"]}x${api.get_fifty_emote()} | ${getuserbest[i]["countmiss"]}${api.get_miss_emote()}\n▸ Score Set ${api.secondto(saniye)}\n`;
+                            thumbnail = "http://s.ppy.sh/a/" + getuser[0]["user_id"] + `?${api.randomnumber(10000)}`
+                            userid = getuser[0]["user_id"]
                             i++;
                         });
-                        console.log(osutopdesc);
                         res(osutopdesc);
+                        res(thumbnail)
+                        res(userid)
                     })
 
+                }).catch(err => {
+                    if (err.message == "Cannot read property 'username' of undefined") {
+                        message.channel.send(`~~${username}~~ **was not found.**`)
+                    }
                 });
-            }).catch(err => {
-                console.log(err)
-            });
+            })
         })
+        try {
+            let osutop = await promise
 
-        let osutop = await promise
-
-        let embed = new Discord.MessageEmbed()
-            .setAuthor(`Top plays for ${username}\n${pp_raw}pp (${pp_rank})`, countryflagicon, "https://osu.ppy.sh/u/" + username)
-            .setDescription(osutop)
-            //.setThumbnail("http://s.ppy.sh/a/" + getuser[0]["user_id"] + `?${api.randomnumber(10000)}`)
-            .setColor(message.member.displayHexColor)
-        console.log(`Osutop Command ms: ${message.createdTimestamp - Date.now()}`)
-        await message.channel.send(embed)
+            let embed = new Discord.MessageEmbed()
+                .setAuthor(`Top plays for ${username}\n${pp_raw}pp (${pp_rank})`, countryflagicon, "https://osu.ppy.sh/u/" + userid)
+                .setDescription(osutop)
+                .setThumbnail(thumbnail)
+                .setColor(message.member.displayHexColor)
+            console.log(username)
+            console.log(`Osutop Command ms: ${message.createdTimestamp - Date.now()}`)
+            await message.channel.send(embed)
+        } catch (err) {
+            console.log(err)
+        }
     },
 };
